9b66f2be91e0b53e08febd98ecbe7f46c57abd00
Pavel Ovsyannikov <pavovsyann@gmail.com>
2023-01-27 13:50:42 +0500

    Added popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,44 @@
-console.log('Привет, мир!');
+const popupElement = document.querySelector('.popup');
+const popupCloseButton = popupElement.querySelector('.popup__close-button');
+const popupOpenButton = document.querySelector('.profile__edit-button')
+const popupSubmit = popupElement.querySelector('popup__submit');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let formElement = popupElement.querySelector('.popup__form');
+let nameInput = popupElement.querySelector('.popup__input_user_name');
+let jobInput = popupElement.querySelector('.popup__input_user_des');
+
+
+const openPopup = function() {
+  popupElement.classList.add('popup_is-opened');
+}
+
+const closePopup = function() {
+  popupElement.classList.remove('popup_is-opened');
+}
+
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    closePopup()
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+function openSubmit() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+
+  openPopup()
+}
+
+popupOpenButton.addEventListener('click', openSubmit)
+
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+
 
